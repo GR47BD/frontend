73839f30c8f41890e12c618e74258c2c4719cab2
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2021-06-28 12:39:47 +0200

    Add special effect to logo

--- src/ui/components/MenuBarComponent.js
+++ src/ui/components/MenuBarComponent.js
@@ -1,10 +1,32 @@
 import m from "mithril";
+import logo from "../../../resources/images/logo.png";
 
 export default class MenuBarComponent {
+	constructor() {
+		this.size = 80;
+	}
+
+	toggleCircle() {
+		document.getElementById("logo-circle").classList.toggle("visible");
+	}
+
+	moveCircle(x, y) {
+		console.log(x, y);
+		x = x - this.size / 2;
+		y = y - this.size / 2;
+
+		const element = document.getElementById("logo-circle");
+		element.style.top = `${y}px`;
+		element.style.left = `${x}px`;
+	}
+
     view() {
         return (	
             <div class="menubar">
-				<div class="logo">Plutode</div>	
+				<div class="logo" onmouseenter={e => this.toggleCircle()} onmouseleave={e => this.toggleCircle()} onmousemove={e => this.moveCircle(e.offsetX, e.offsetY)}>
+					<div class="effect"><div class="circle" id="logo-circle"></div></div>
+					<img class="image" src={logo}></img>
+				</div>	
 				<div class="links">
 					<div class="link" onclick={() => m.route.set("/")}>Home</div>
 					<div class="link" onclick={() => m.route.set("/visualize")}>Visualize</div>
