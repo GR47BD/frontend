bb36f48a396336ece90bdf15880aea6fb0f59474
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2021-06-28 13:09:34 +0200

    Fix security alerts

--- build/build.dev.js
+++ build/build.dev.js
@@ -5,9 +5,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const port = 8080;
 const config = require("./webpack.dev.conf");
 const compiler = webpack(config);
-const server = new WebpackDevServer(compiler, {
-	contentBase: path.join(__dirname, '../dist')
-});
+const server = new WebpackDevServer(compiler, {});
 
 // Start the development server on the specified port and 'localhost'.
 server.listen(port, 'localhost', function (err) {
